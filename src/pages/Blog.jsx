7903fc01adb8ec5a2f99d1774a7344c00fff3196
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-18 16:56:44 -0300

    update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -9,7 +9,7 @@ export default function Blog() {
         <>
             <Header />
             <SectionTitleBlog />
-            <FormEmail />
+            {/* <FormEmail /> */}
             <Footer />
 
         </>
