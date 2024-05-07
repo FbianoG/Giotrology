084fa8c8c63667a2e638ee71abc60a259f86bcf9
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-05-07 18:56:57 -0300

    update Header.css and Header.jsx

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -1,8 +1,21 @@
+import { useState, useEffect, useRef } from "react"
 import "./Header.css"
 
 
 
 export default function Header() {
+    const [showMenu, setShowMenu] = useState(true)
+
+    function changeMenu() {
+        if (showMenu) setShowMenu(false)
+        else setShowMenu(true)
+    }
+
+    useEffect(() => {
+        if (window.innerWidth <= 767) {
+            setShowMenu(false);
+        }
+    }, []);
 
     return (
         <header>
@@ -10,12 +23,18 @@ export default function Header() {
             <div className="logo">
                 Giotrology
             </div>
-            <nav>
-                <a href="/">Início</a>
-                <a href="/articles">Artigos</a>
-                <a href="#sectionSign">Signos</a>
-                <a href="#sectionPrice">Mapa Astral</a>
-            </nav>
+            {showMenu &&
+                <nav >
+                    <a href="/">Início</a>
+                    <a href="/articles">Artigos</a>
+                    <a href="#sectionSign">Signos</a>
+                    <a href="/payment">Mapa Astral</a>
+                </nav>
+            }
+            <button className="header__btnMenu" onClick={changeMenu}>
+                {!showMenu && <i className="fa-solid fa-bars"></i>}
+                {showMenu && <i className="fa-solid fa-xmark"></i>}
+            </button>
 
         </header>
     )
