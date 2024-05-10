93eeb31db95eb36b8974fdb6122bb21c7d923f56
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-05-09 21:36:59 -0300

    update 4 files and create 1 file

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import './Articles.css'
 import Stars from "../components/Common/Stars";
 import Footer from "../components/Shared/Footer";
 import Header from "../components/Shared/Header";
@@ -12,6 +13,7 @@ export default function Articles() {
 
     async function getArticles() {
         const response = await axios.post(`${urlBack}/getArticles`)
+        console.log(response);
         setArticles(response.data)
     }
 
@@ -23,15 +25,15 @@ export default function Articles() {
         <>
             <Header />
             <div className="content">
-                <h1 className="title">Todos os Artigos</h1>
-                <div className="listBlog">
+                <h1 className="list__articles-title">Todos os Artigos</h1>
+                <div className="list__articles">
                     {Articles &&
                         Articles.map(element => (
                             <CardArticle key={element._id} data={element} />
                         ))
                     }
                 </div>
-                <CardArticle />
+
                 <Stars />
             </div>
             <Footer />
